01262c8394c509758c302f80445ac34d8660a9ab
Abdul Wahab <148485953+AbdulWahab740@users.noreply.github.com>
2024-08-06 15:54:31 -0700

    urls.py

--- blindAssist/blindapp/urls.py
+++ blindAssist/blindapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import home, upload_image
+from .views import home, upload_image,get_speech_text
 
 urlpatterns = [
     path("",home,name='home'),
+
+    path('get_speech_text/', get_speech_text, name='get_speech_text'),
+
     path('upload_image/', upload_image, name='upload_image'),
 ]
